47feec96eda38fe1fc0c7c5c4eba40df98b10aac
M48✌🚱 <100668901+Cher-IS-7@users.noreply.github.com>
2024-08-14 23:51:26 +0400

    Add files via upload

---
+++ Termux Cheatsheet/termuxcheats.sh
@@ -0,0 +1,11 @@
+echo -e "\033[31mTermux Cheat Sheet \033[0m";
+
+# Navigating, changing directories and managing files:
+echo -e "\n\033[36mNavigating, changing directories and managing files:\033[0m";
+printf "\033[1;33m    View files and folders: ls \n    Open folder: cd *folder_name \n    Go up: cd .. \n    Create folder: mkdir *folder_name \n    Create file: vim *file_name \n    View file: vim *file_name \n    Delete file: rm *file_name \n    Delete folder: rmdir *folder_name \n    Move file: mv *file_name *folder_name \n    Move folder: mv *folder_name *folder_name \n    Copy file: cp *file_name *folder_name \n    Run bash script: bash *file_name \n    Other way to run bash script: *file_name.sh\033[0m"
+
+# Install from Github and Run Python scripts:
+echo -e "\n\033[36mInstall from Github and Run Python scripts:\033[0m";
+printf "\033[1;33m    Install from Github: wget https://raw.githubusercontent.com/Cher-IS-7/apple.py/main/apple.py";
+printf "\n    Run: python apple.py\033[0m"
+
